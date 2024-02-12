51b98dc848f2b7cabdb28858ea1cf2ab778cca68
mehdidiah <71126054+mehdidiah@users.noreply.github.com>
2024-02-12 09:41:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,6 +27,12 @@ def meteo():
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
+
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
